4e9ba414eba3c2122f07295df10f5a0022287519
bbkbarbar <bbk.barbar@gmail.com>
2016-07-05 12:00:36 +0200

    .

--- remote-server/Commands.java
+++ remote-server/Commands.java
@@ -5,6 +5,8 @@ public class Commands {
 	public static final String GET_TEMP = "readTemp";
 	
 	public static final String GET_CLIENT_COUNT = "client_count";
+
+	public static final String GET_CLIENT_LIST = "client_list";
 	
 	public static final String STOP_SERVER = "stop_server";
 
